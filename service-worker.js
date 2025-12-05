3436e25ce6e2393f650bc5a5381f8c9b8bb94024
DavidSB73 <davidstephenbryan@gmail.com>
2025-12-05 10:06:16 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,23 @@
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open("mathquiz-cache").then((cache) => {
-      return cache.addAll([
-        "./index.html",
-      ]);
+const CACHE_NAME = 'mathquiz-v1';
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
-});
